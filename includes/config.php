40cfb846a16e99d1eaf001880f27ca61e9d5bcd4
Addi-11 <saddi.5131@gmail.com>
2021-11-14 20:09:57 +0530

    database connected

---
+++ includes/config.php
@@ -0,0 +1,10 @@
+<?php
+$servername = "localhost:3307";
+$username = "root1";
+$password = "password";
+$dbname = "e_classrooms";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+?>
