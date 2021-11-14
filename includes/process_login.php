392a61d2387a3dc179d23d13139ebd3c4d5c528c
Addi-11 <saddi.5131@gmail.com>
2021-11-14 21:05:07 +0530

    login connected+verified with DB

--- includes/process_login.php
+++ includes/process_login.php
@@ -1,4 +1,31 @@
 <?php
-session_start();
-echo "login route works";
+require_once('config.php');
+
+$login_id = $_POST['login-id'];
+$login_pass = $_POST['login-password'];
+
+
+$table_name = 'users';
+
+$query = mysqli_query($conn, "select * from ".$table_name." where email='$login_id' and password='$login_pass'");
+$result = mysqli_fetch_array($query, MYSQLI_ASSOC);
+
+if (($result > 0)) {
+    $profession = $result['profession'];
+    if ($profession == "teacher") {
+        session_start();
+        // $_SESSION["id"] = $result['email'];
+        $_SESSION["type"] = $profession;
+        header('location: ../views/teacher/home.php');
+    } 
+    else if ($profession == "Student") {
+        session_start();
+        // $_SESSION["id"] = $result['email'];
+        $_SESSION["type"] = $profession;
+        header('location:  ../views/student/home.php');
+    }
+} else {
+    header('location: ../views/login.php');
+}
+
 ?>
