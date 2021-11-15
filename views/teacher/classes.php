c3bf81825b07b37b8f0924150b85525c8ec20240
Addi-11 <saddi.5131@gmail.com>
2021-11-16 03:44:34 +0530

    adjusted the create class button

--- views/teacher/classes.php
+++ views/teacher/classes.php
@@ -2,26 +2,20 @@
 include_once 'sidebar.php';
 ?>
 <div class="left-container">
-    <div class="ui grid">
-        <div class="sixteeen wide column">
-            <form action="create_class.php">
-                <button class="ui right floated button">
-                    Create Classroom
-                </button>
-            </form>
+    <div class="ui basic segment">
+        <form action="create_class.php">
+            <button class="ui right floated button">
+                <i class="plus icon"></i>
+                Create Classroom
+            </button>
+        </form>
+    </div><br>
+    <div class="ui basic segment">
+        <div class="ui cards">
+            <?php
+            include_once '../../includes/process_target_class.php';
+            ?>
         </div>
-        <div class="sixteen wide column">
-
-        </div>
-    </div>
-    <div class="ui cards">
-        <?php
-
-        include_once '../../includes/process_target_class.php';
-
-        ?>
-            
-
     </div>
 </div>
 
