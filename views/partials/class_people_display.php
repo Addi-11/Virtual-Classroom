673129419161ffe13737417acd79711d8a308574
Addi-11 <saddi.5131@gmail.com>
2021-11-16 15:50:19 +0530

    class people display code modulaized

---
+++ views/partials/class_people_display.php
@@ -0,0 +1,19 @@
+<div class="ui bottom attached segment">
+    <div class="ui container">
+
+        <div class="ui header">
+            <span style="font-size:30px">Teachers</span>
+            <div class="ui right floated tiny header">
+                <i class="user plus icon"></i>
+            </div>
+        </div>
+        <div class="ui divider"></div>
+
+        <div class="ui header">
+            <span style="font-size:30px">Student</span>
+            <span class="ui right floated tiny header">
+                <i class="user plus icon"></i>
+            </span>
+        </div>
+    </div>
+</div>
--- views/student/class_people.php
+++ views/student/class_people.php
@@ -13,32 +13,12 @@ include_once 'sidebar.php';
             People
         </a>
     </div>
-    <div class="ui bottom attached segment">
-        <div class="ui container">
-
-            <div class="ui header">
-                <span style="font-size:30px">Teachers</span>
-                <div class="ui right floated tiny header">
-                    <i class="user plus icon"></i>
-                </div>
-            </div>
-            <div class="ui divider"></div>
-
-            <div class="ui header">
-                <span style="font-size:30px">Student</span>
-                <span class="ui right floated tiny header">
-                    <i class="user plus icon"></i>
-                </span>
-            </div>
-        </div>
-    </div>
+    <?php include_once '../partials/class_people_display.php';?>
 
 </div>
 
 
 </body>
-
-
 <script src="../../js/sidebar.js" type="text/javascript"></script>
 
 </html>
