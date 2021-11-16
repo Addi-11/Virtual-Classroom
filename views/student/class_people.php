edf6be911a18bfabe1addc6a8c644144d38b7b44
Addi-11 <saddi.5131@gmail.com>
2021-11-16 18:02:25 +0530

    student-class link errors fixed

--- views/student/class_people.php
+++ views/student/class_people.php
@@ -1,19 +1,20 @@
 <?php
 include_once 'sidebar.php';
+$class_code = $_GET["class_code"];
 ?>
 <div class="left-container">
     <div class="ui top attached tabular menu">
-        <a class="item" href="class_stream.php">
+        <a class="item" href="class_stream.php?class_code=<?php echo $class_code; ?>">
             Stream
         </a>
-        <a class="item" href="classwork.php">
+        <a class="item" href="classwork.php?class_code=<?php echo $class_code; ?>">
             Classwork
         </a>
-        <a class="active item" href="class_people.php">
+        <a class="active item" href="class_people.php?class_code=<?php echo $class_code; ?>">
             People
         </a>
     </div>
-    <?php include_once '../partials/class_people_display.php';?>
+    <?php include_once '../partials/class_people_display.php'; ?>
 
 </div>
 
--- views/student/class_stream.php
+++ views/student/class_stream.php
@@ -1,24 +1,22 @@
 <?php
-    include_once 'sidebar.php';
-    $class_code = $_GET["class_code"];
-    include_once '../../includes/config.php';
-    $query = mysqli_query($conn, "select * from classrooms where classroom_code='$class_code'");
-    $result = mysqli_fetch_array($query, MYSQLI_ASSOC);
+include_once 'sidebar.php';
+$class_code = $_GET["class_code"];
+include_once '../../includes/config.php';
+$query = mysqli_query($conn, "select * from classrooms where classroom_code='$class_code'");
+$result = mysqli_fetch_array($query, MYSQLI_ASSOC);
 ?>
 
 <div class="left-container">
     <div class="ui top attached tabular menu">
-        <a class="active item" href="class_stream.php">
+        <a class="active item" href="class_stream.php?class_code=<?php echo $class_code; ?>">
             Stream
         </a>
-        <a class="item" href="classwork.php">
+        <a class="item" href="classwork.php?class_code=<?php echo $class_code; ?>">
             Classwork
         </a>
-        <a class="item" href="class_people.php">
+        <a class="item" href="class_people.php?class_code=<?php echo $class_code; ?>">
             People
         </a>
     </div>
-    <?php
-    include_once '../partials/class_stream_display.php';
-    ?>
+    <?php include_once '../partials/class_stream_display.php';?>
 </div>
