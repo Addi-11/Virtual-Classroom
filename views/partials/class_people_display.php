e0234ce85d986881497cb419bfdaaa9e9422a873
Addi-11 <saddi.5131@gmail.com>
2021-11-16 18:25:04 +0530

    viewing class people - done - not stylish

--- views/partials/class_people_display.php
+++ views/partials/class_people_display.php
@@ -1,3 +1,23 @@
+<?php
+include_once '../../includes/config.php';
+// get all students for given class
+$student_query = mysqli_query($conn, "select name from users where id in (select user_id from class_student where classroom_code='$class_code')");
+$student_result = mysqli_fetch_all($student_query, MYSQLI_ASSOC);
+
+// get all teachers for given class
+// select name from users where id in (select user_id from classrooms where classroom_code=session_classcode);
+$teacher_query = mysqli_query($conn,"select name from users where id in (select user_id from classrooms where classroom_code='$class_code')");
+$teacher_result=mysqli_fetch_array($teacher_query,MYSQLI_ASSOC);
+
+foreach($student_result as $s){
+    echo implode(" ",$s).'<br>';
+}
+echo '<br>';
+foreach ($teacher_result as $s) {
+    echo $s . '<br>';
+}
+?>
+
 <div class="ui bottom attached segment">
     <div class="ui container">
 
