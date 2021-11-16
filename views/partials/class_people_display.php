280161cac4c3973b76954da489dcd76533cf9830
Addi-11 <saddi.5131@gmail.com>
2021-11-16 19:52:41 +0530

    class-people ui created

--- views/partials/class_people_display.php
+++ views/partials/class_people_display.php
@@ -5,35 +5,53 @@ $student_query = mysqli_query($conn, "select name from users where id in (select
 $student_result = mysqli_fetch_all($student_query, MYSQLI_ASSOC);
 
 // get all teachers for given class
-// select name from users where id in (select user_id from classrooms where classroom_code=session_classcode);
-$teacher_query = mysqli_query($conn,"select name from users where id in (select user_id from classrooms where classroom_code='$class_code')");
-$teacher_result=mysqli_fetch_array($teacher_query,MYSQLI_ASSOC);
-
-foreach($student_result as $s){
-    echo implode(" ",$s).'<br>';
-}
-echo '<br>';
-foreach ($teacher_result as $s) {
-    echo $s . '<br>';
-}
+$teacher_query = mysqli_query($conn, "select name from users where id in (select user_id from classrooms where classroom_code='$class_code')");
+$teacher_result = mysqli_fetch_all($teacher_query, MYSQLI_ASSOC);
 ?>
 
 <div class="ui bottom attached segment">
-    <div class="ui container">
+    <br>
+    <div class="ui grid">
+        <div class="ui sixteen column grid">
+            <div class="ui container">
+                <div class="ui header">
+                    <span style="font-size:25px">Teachers</span>
+                    <div class="ui right floated tiny header">
+                        <i class="user plus icon"></i>
+                    </div>
+                </div>
+                <div class="ui middle aligned list">
+                    <?php foreach ($student_result as $student) { ?>
+                        <div class="item">
+                            <img class="ui avatar image" src="../../images/teacher.png">
+                            &ensp;
+                            <span style="font-size:18px">
+                                <?= implode(" ", $student) ?>
+                            </span>
+                        </div>
+                    <?php } ?>
+                </div>
 
-        <div class="ui header">
-            <span style="font-size:30px">Teachers</span>
-            <div class="ui right floated tiny header">
-                <i class="user plus icon"></i>
+                <div class="ui divider"></div>
+
+                <div class="ui header">
+                    <span style="font-size:25px">Students</span>
+                    <div class="ui right floated tiny header">
+                        <i class="user plus icon"></i>
+                    </div>
+                </div>
+                <div class="ui middle aligned list">
+                    <?php foreach ($teacher_result as $teacher) { ?>
+                        <div class="item">
+                            <img class="ui avatar image" src="../../images/student.jpg">
+                            &ensp;
+                            <span style="font-size:18px">
+                                <?= implode(" ", $teacher) ?>
+                            </span>
+                        </div>
+                    <?php } ?>
+                </div>
             </div>
-        </div>
-        <div class="ui divider"></div>
-
-        <div class="ui header">
-            <span style="font-size:30px">Student</span>
-            <span class="ui right floated tiny header">
-                <i class="user plus icon"></i>
-            </span>
         </div>
     </div>
 </div>
