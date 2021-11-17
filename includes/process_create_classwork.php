63c58aa5573b650b494c8dc3dd5486e5717eb80c
Addi-11 <saddi.5131@gmail.com>
2021-11-17 22:40:58 +0530

    classwork info gets stored in the DB with+without uploaded files

--- includes/process_create_classwork.php
+++ includes/process_create_classwork.php
@@ -17,20 +17,39 @@ $create_date= date('Y-m-d H:i:s');
 $targetfolder = "../uploads/";
 $targetfolder = $targetfolder . $file_id;
 
+$table_name='files';
 
+$url_classwork = '../views/teacher/classwork.php?class_code='.$classroom_code;
+$url_create_classwork = '../views/teacher/create_classwork.php?class_code='.$classroom_code;
 
-
-if(move_uploaded_file($_FILES['file_name']['tmp_name'], $targetfolder))
-{
-    // echo "The file ". basename( $_FILES['file_name']['name']). " is uploaded";
-    $query="Insert into files(classroom_code, uploaded_file_name,uploaded_file_size,file_id,classwork_title,classwork_inst,classwork_marks,classwork_topic,due_date,create_date) values ('$classroom_code', '$uploaded_file_name','$uploaded_file_size','$file_id','$classwork_title','$classwork_inst','$classwork_marks','$classwork_topic','$due_date','$create_date')";
-    if(mysqli_query($conn,$query)){
-        echo "File uploaded successfully";
-
-    }else{
-        echo "Failed to upload file";
+// checking for 
+if($classwork_title==NULL){
+    echo '<script>alert("Empty classwork not allowed.")</script>';
+    // header('location: '.$url_create_classwork);
+}else{
+    // if file is uploaded - move to folder
+    if(move_uploaded_file($_FILES['file_name']['tmp_name'], $targetfolder))
+    {
+        // echo "The file ". basename( $_FILES['file_name']['name']). " is uploaded";
+        $query="Insert into ". $table_name."(classroom_code, uploaded_file_name,uploaded_file_size,file_id,classwork_title,classwork_inst,classwork_marks,classwork_topic,due_date,create_date) values ('$classroom_code', '$uploaded_file_name','$uploaded_file_size','$file_id','$classwork_title','$classwork_inst','$classwork_marks','$classwork_topic','$due_date','$create_date')";
+        if(mysqli_query($conn,$query)){
+            echo "File uploaded successfully";
+            header('location: '.$url_classwork);
+        }else{
+            echo "Failed to upload file";
+            header('location: '.$url_create_classwork);
+        }
     }
-}
-else {
-    echo "Problem uploading file";
+    // if no file is uploaded
+    else {
+        $query="Insert into ".$table_name."(classroom_code,file_id,classwork_title,classwork_inst,classwork_marks,classwork_topic,due_date,create_date) values ('$classroom_code','$file_id','$classwork_title','$classwork_inst','$classwork_marks','$classwork_topic','$due_date','$create_date')";
+        if(mysqli_query($conn, $query)){
+            
+            // echo "File uploaded successfully";
+            header('location: '.$url_classwork);
+        }else{
+            // echo "Failed to upload file";
+            header('location: '.$url_create_classwork);
+        }
+    }
 }
