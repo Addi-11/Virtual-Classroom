58c74824ff73d70ace5fce4f6e8dff845e401ab2
Addi-11 <saddi.5131@gmail.com>
2021-11-18 22:05:42 +0530

    student grading + asgn viewing ui done

--- views/teacher/grading.php
+++ views/teacher/grading.php
@@ -8,34 +8,37 @@ $result = mysqli_fetch_all($query, MYSQLI_ASSOC);
 
 
 foreach ($result as $student) {
-    include '../error.php';
-    echo $student['name']; ?>
-    <form method="post">
-        <input type="hidden" name='file_id' value="<?= $student['submission_file_id'] ?>">
-        <input type="submit" name="download_student_button" value="Download Files" />
-    </form>
-    <form method="post">
-        <input type="hidden" name='file_id' value="<?= $student['submission_file_id'] ?>">
-        <input type="text" name='marks'>
-        <input type="submit" name="submit_marks" value="Score" />
-    </form>
-<?php 
-    // if($_SESSION['error']=='Success') {echo "success".'<br>';}
-    // else if($_SESSION['error']!='0'){echo "Failed to update score."."<br>";}
+?>
+    <div class="ui grid">
+        <div class="eight wide column">
+            <form method="post">
+                <div class="ui grid">
+                    <div class="ten wide column">
+                        <div class="item">
+                            <img class="ui avatar image" src="../../images/student.jpg">
+                        <span style="text-transform:uppercase;"><?= $student['name'] ?></span>
+                        </div>
+                    </div>
+                    <div class="six wide column">
+                        <input type="hidden" name='file_id' value="<?= $student['submission_file_id'] ?>">
+                        <input type="submit" name="download_student_button" value="Download" />
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="eight wide column">
+            <form method="post">
+                <input type="hidden" name='file_id' value="<?= $student['submission_file_id'] ?>">
+                <div class="ui mini input">
+                    <input type="text" name='marks' placeholder="Enter Marks">
+                </div>
+                <input type="submit" name="submit_marks" value="Score" />
+            </form>
+        </div>
+    </div>
+    <div class="ui divider"></div>
+<?php
+    // if($_SESSION['error']=='Success') {echo "success";}
+    // else if($_SESSION['error']!='0'){echo "Failed to update score.";}
     // $_SESSION['error']='0';
- } ?>
-
-
-<!-- // <div class="ui middle aligned list">
-//     <?php //foreach ($student_submission as $student) { 
-        ?>
-//         <div class="item">
-//             <img class="ui avatar image" src="../../images/student.jpg">
-//             &ensp;
-//             <span style="font-size:18px">
-                
-//             </span>
-//         </div>
-//     <?php //} 
-        ?>
-// </div> -->
+} ?>
--- views/teacher/view_asgn_classwork.php
+++ views/teacher/view_asgn_classwork.php
@@ -1,29 +1,29 @@
 <?php
-    include_once 'sidebar.php';
-    $file_id = $_GET['file_id'];
-    include_once '../../includes/config.php';
+include_once 'sidebar.php';
+$file_id = $_GET['file_id'];
+include_once '../../includes/config.php';
 
-    // get all info from files
-    $query = mysqli_query($conn, "Select * from files where file_id='$file_id'");
-    $result = mysqli_fetch_array($query, MYSQLI_ASSOC);
+// get all info from files
+$query = mysqli_query($conn, "Select * from files where file_id='$file_id'");
+$result = mysqli_fetch_array($query, MYSQLI_ASSOC);
 
-    // get classroomname from classcoe
-    $class_code = $result['classroom_code'];
-    $query = mysqli_query($conn, "Select subject_name from classrooms where classroom_code='$class_code'");
-    $classroom_name = mysqli_fetch_array($query, MYSQLI_ASSOC);
+// get classroomname from classcoe
+$class_code = $result['classroom_code'];
+$query = mysqli_query($conn, "Select subject_name from classrooms where classroom_code='$class_code'");
+$classroom_name = mysqli_fetch_array($query, MYSQLI_ASSOC);
 
-    if ($result['due_date'] == '0000-00-00 00:00:00') {
-        $result['due_date'] = 'None';
-    }
-    if (isset($_POST['download_button'])) {
-        include_once '../../includes/download_teacher.php';
-    }
-    if (isset($_POST['download_student_button'])) {
-        include_once '../../includes/download_student.php';
-    }
-    if (isset($_POST['submit_marks'])) {
-        include_once '../../includes/process_grading.php';
-    }
+if ($result['due_date'] == '0000-00-00 00:00:00') {
+    $result['due_date'] = 'None';
+}
+if (isset($_POST['download_button'])) {
+    include_once '../../includes/download_teacher.php';
+}
+if (isset($_POST['download_student_button'])) {
+    include_once '../../includes/download_student.php';
+}
+if (isset($_POST['submit_marks'])) {
+    include_once '../../includes/process_grading.php';
+}
 ?>
 
 <div class="left-container">
@@ -34,7 +34,7 @@
                 Back
             </a>
         </div>
-        <div class="ui eleven wide column">
+        <div class="ui ten wide column">
             <h1 class="ui huge header">
                 <i class="book icon"></i>
                 <div class="content">
@@ -58,15 +58,25 @@
                     <p><?= $result['classwork_inst'] ?></p>
                 </div>
                 <!-- Download option for classwork files -->
-                <?php if($result['uploaded_file_name'] !='' and $result['uploaded_file_name']!=NULL){?>
+                <?php if ($result['uploaded_file_name'] != '' and $result['uploaded_file_name'] != NULL) { ?>
                     <form method="post">
                         <input type="submit" name="download_button" value="Download Files" />
                     </form>
                 <?php } ?>
             </div>
         </div>
-        <div class="five wide column">
-            <?php include_once 'grading.php'?>
+        <div class="six wide column">
+            <div class="ui segment">
+                <div class="sixteen wide column">
+                    <div class="ui large header">
+                        <i class="folder open icon"></i>
+                        Student Submissions
+                    </div>
+                </div>
+                <div class="ui divider"></div>
+                <br>
+                <?php include_once 'grading.php' ?>
+            </div>
         </div>
     </div>
 </div>
