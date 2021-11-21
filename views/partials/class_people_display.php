10db7265a4eab5bb7d6e52daa7adddf2517c0b3b
Addi-11 <saddi.5131@gmail.com>
2021-11-22 03:26:56 +0530

    added vaccination status

--- views/partials/class_people_display.php
+++ views/partials/class_people_display.php
@@ -1,12 +1,18 @@
 <?php
 include_once '../../includes/config.php';
 // get all students for given class
-$student_query = mysqli_query($conn, "select name from users where id in (select user_id from class_student where classroom_code='$class_code')");
+$student_query = mysqli_query($conn, "select * from users where id in (select user_id from class_student where classroom_code='$class_code')");
 $student_result = mysqli_fetch_all($student_query, MYSQLI_ASSOC);
 
 // get all teachers for given class
 $teacher_query = mysqli_query($conn, "select name from users where id in (select user_id from classrooms where classroom_code='$class_code')");
 $teacher_result = mysqli_fetch_all($teacher_query, MYSQLI_ASSOC);
+$image_url="../../images/".$_SESSION['profession'].".png";
+
+if(isset($_SESSION['image_extension'])){
+    $image_url="../../images/users/".$_SESSION['id'] . ".".$_SESSION['image_extension'];
+}
+
 ?>
 
 <div class="ui bottom attached segment">
@@ -23,10 +29,10 @@ $teacher_result = mysqli_fetch_all($teacher_query, MYSQLI_ASSOC);
                 <div class="ui middle aligned list">
                     <?php foreach ($teacher_result as $teacher) { ?>
                         <div class="item">
-                            <img class="ui avatar image" src="../../images/teacher.png">
+                            <img class="ui avatar image" src="<?=$image_url?>">
                             &ensp;
                             <span style="font-size:18px">
-                                <?= implode(" ", $teacher) ?>
+                                <?= $teacher['name'] ?>
                             </span>
                         </div>
                     <?php } ?>
@@ -41,13 +47,26 @@ $teacher_result = mysqli_fetch_all($teacher_query, MYSQLI_ASSOC);
                     </div>
                 </div>
                 <div class="ui middle aligned list">
-                    <?php foreach ($student_result as $student) { ?>
+                    <?php foreach ($student_result as $student) {
+                        $color = "";
+                        // vaccine status color setting
+                        if ($student['vaccine_stat'] == 'not-vaccine') {
+                            $color = "red";
+                        } else if ($student['vaccine_stat'] == 'full-vaccine') {
+                            $color = "green";
+                        } else if ($student['vaccine_stat'] == 'partial-vaccine') {
+                            $color = "yellow";
+                        }
+                    ?>
                         <div class="item">
-                            <img class="ui avatar image" src="../../images/student.jpg">
+                            <img class="ui avatar image" src="<?=$image_url?>">
                             &ensp;
                             <span style="font-size:18px">
-                                <?= implode(" ", $student) ?>
+                                <?= $student['name'] ?>
                             </span>
+                            <div class="ui right floated <?=$color?> tiny header">
+                                <?=$student['vaccine_stat']?>
+                            </div>
                         </div>
                     <?php } ?>
                 </div>
